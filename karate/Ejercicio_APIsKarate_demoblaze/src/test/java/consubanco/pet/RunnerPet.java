6d26e482202c8b54ee1761324bb6703d24f6c612
Jorge Ivan Gomez Restrepo <jorggors@suramericana.com.co>
2024-07-24 19:34:43 -0500

    Eliminacion archivos sobrantes

--- karate/Ejercicio_APIsKarate_demoblaze/src/test/java/consubanco/pet/RunnerPet.java
+++
@@ -1,9 +0,0 @@
-package consubanco.pet;
-
-import com.intuit.karate.junit5.Karate;
-
-public class RunnerPet {
-    @Karate.Test
-    Karate testGet() { return Karate.run("pet-post").relativeTo(getClass());
-    }
-}
